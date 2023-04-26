4dcc71e45d33387d864b825d10f1283fb6710ff9
Thomas Wilme <thomas.wilme@wsu.edu>
2023-04-26 08:31:08 -0700

    Added a test function to test if boids are given random colors.

--- PA-9/Test.cpp
+++ PA-9/Test.cpp
@@ -1,11 +1,13 @@
 #include "Test.h"
 
+// Runs all the test functions.
 void Test::runTest()
 {
 	testInit();
+	testBoidColorConstructor();
 }
 
-
+// Testing of the BoidManager.init function. Tests if the correct number of nodes are created.
 void Test::testInit()
 {
 	BoidManager testManager(5, 2, 0.02, 30, 0.005, 70, 0.004, 200, 0.3, 50, 30, 0.001);
@@ -23,4 +25,15 @@ void Test::testInit()
 
 	if (nodeCount == 5) std::cout << "Test init passed." << std::endl;
 	else std::cout << "Test init failed" << std::endl;
+}
+
+// Testing of the random color feature in the Boid constructor. Tests to make sure that random colors are applied.
+void Test::testBoidColorConstructor()
+{
+	Boid b1(sf::Vector2f(0, 0), sf::Vector2f(0, 0), 2, 0);
+	Boid b2(sf::Vector2f(0, 0), sf::Vector2f(0, 0), 2, 0);
+	Boid b3(sf::Vector2f(0, 0), sf::Vector2f(0, 0), 2, 0);
+
+	if (b1.getFillColor() == b2.getFillColor() && b2.getFillColor() == b3.getFillColor()) std::cout << "Test boid color constructor failed." << std::endl;
+	else std::cout << "Test boid color constructor passed." << std::endl;
 }
--- PA-9/Test.h
+++ PA-9/Test.h
@@ -6,10 +6,14 @@
 class Test
 {
 public:
+	// Runs all the test functions.
 	void runTest();
 
-
+	// Testing of the BoidManager.init function. Tests if the correct number of nodes are created.
 	void testInit();
+
+	// Testing of the random color feature in the Boid constructor. Tests to make sure that random colors are applied.
+	void testBoidColorConstructor();
 
 
 
